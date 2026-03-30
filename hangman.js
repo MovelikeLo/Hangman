76b0dbefc27a8490349dac10ba363b4dccff30b6
MovelikeLo <loicrobert640@gmail.com>
2026-03-30 10:52:21 -0400

    Made the nessacery function to make the game able to work

--- hangman.js
+++ hangman.js
@@ -1,18 +1,35 @@
 const POSSIBLE_WORDS = ["obdurate", "versimilitude", "defenestrate", "obsequious", "dissonant", "toady", "idempotent"];
-var clue = "";
-var guessesLeft = 6;
-var guessedLetters = [];
+var word = "";
+var guesses = "";
 
 let newGame = function() {
     ranmdomIndex = parseInt(Math.random() * POSSIBLE_WORDS.length);
     word = POSSIBLE_WORDS[ranmdomIndex];
-    
+    guesses = "";
+    updatePage();
+}
+let updatePage = function() {
     let clueString = "";
     for (let i = 0; i < word.length; i++) {
-        clueString += "_";
+        var currentLetter = word.charAt(i);
+        if (guesses.indexOf(currentLetter) >= 0) {
+            clueString += currentLetter + " ";
+        } else {
+            clueString += "_ ";
+        }
     }   
-    
     
     let clue = document.getElementById("clue");
     clue.textContent = clueString;
+
+    let guessArea = document.getElementById("guesses");
+    guessArea.textContent = "Guesses: " + guesses;
+};
+
+
+let guessLetter = function() {
+    let input = document.getElementById("guess");
+    let letter = input.value;
+    guesses+=letter;
+    updatePage();
 }
