d8a4a6f642484e76ad66bb02623521a37ead1bb9
MovelikeLo <loicrobert640@gmail.com>
2026-04-01 10:33:54 -0400

    make image change on wrong guess

--- hangman.js
+++ hangman.js
@@ -1,8 +1,11 @@
 const POSSIBLE_WORDS = ["obdurate", "versimilitude", "defenestrate", "obsequious", "dissonant", "toady", "idempotent"];
 var word = "";
 var guesses = "";
+var guessCount;
+const MAX_GUESSES = 6;
 
 let newGame = function() {
+    guessCount = MAX_GUESSES;
     ranmdomIndex = parseInt(Math.random() * POSSIBLE_WORDS.length);
     word = POSSIBLE_WORDS[ranmdomIndex];
     guesses = "";
@@ -24,12 +27,18 @@ let updatePage = function() {
 
     let guessArea = document.getElementById("guesses");
     guessArea.textContent = "Guesses: " + guesses;
+    let image = document.getElementById("hangmanpic");
+    image.src = 'hangman${guessCount}.gif';
 };
 
 
 let guessLetter = function() {
     let input = document.getElementById("guess");
     let letter = input.value;
+    if(word.indexOf(letter) < 0) {
+        guessCount--;
+    }
+    letter = letter.toLowerCase();
     guesses+=letter;
     updatePage();
 }
